8c5d6367c7c509bd90e165ce095f5d5d2ec1c790
raravilov <raravilov@gmail.com>
2019-02-20 15:27:49 +0300

    ex2

--- homeworkfile.py
+++ homeworkfile.py
@@ -5,6 +5,7 @@ dishes_list = []
 dishes_dict_list = {}
 space_item = []
 
+print ('Решение задачи №1')
 i = 0
 сount_elements = 0
 
@@ -31,21 +32,36 @@ def ListToDict(num):
     ingredients_dict = { 'ingridient_name':list_ingredients_new[0], 'quantity':list_ingredients_new[1], 'measure':list_ingredients_new[2]}
     return ingredients_dict
 
-dishes_count = 0
+dishes_count = 0 #порядковый номер блюда
 j = 2
 
 for dishes in cook_book:
-    while j <= space_item[dishes_count]:
+    while j <= space_item[dishes_count]: #понимаем количество строк ингредиентов блюда за счет того что знаем порядковый номер пустой строки разделяющий блюда
         if j != space_item[dishes_count]:
-            dishes_list.append(ListToDict(j))
+            dishes_list.append(ListToDict(j))  #список буфер, в который собираем ингредиенты одного блюда
         else:
             j += 3
-            dishes_count += 1
-            dishes_dict_list[dishes_count] = copy.deepcopy(dishes_list)
+            dishes_count += 1 #переход на следующее блюдо
+            dishes_dict_list[dishes_count] = copy.deepcopy(dishes_list) #перед очищением списка буфера, делаем копию
             cook_book[dishes] = dishes_dict_list[dishes_count]
-            dishes_list.clear()
+            dishes_list.clear() #при переходе на следующее блюдо очищаем список буфер
             break
         j += 1
 
 
-print (cook_book)
+print (cook_book) #вывод результата решения первой задачи
+
+print ('Решение Задачи №2')
+input_list = ['Запеченный картофель', 'Омлет']
+input_person_count = 2
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    g = 0
+    for recipe in dishes:
+        while g <len(cook_book[recipe]):
+            shop_list[cook_book[recipe][g]['ingridient_name']] = {'measure': cook_book[recipe][g]['measure'], 'quantity': int(cook_book[recipe][g]['quantity']) * person_count}
+            g += 1
+        g = 0
+    print (shop_list)
+
+get_shop_list_by_dishes(input_list, input_person_count)
